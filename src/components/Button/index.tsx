65d23b44191103e6e6b82d7bf980dcdc0efcd0ac
Sven65 <Sven65@users.noreply.github.com>
2024-01-09 13:27:50 +0100

    More shit

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,23 @@
-import { h, FunctionComponent, ComponentChildren, Component } from "preact";
+import { h, FunctionComponent, ComponentChildren } from "preact";
+import { TailwindBackgroundColor, TailwindBackgroundColors, TailwindTextColor, TailwindTextColors } from "../../types/tailwind/colors";
 
 interface Props extends h.JSX.HTMLAttributes<HTMLButtonElement> {
 	id?: string,
 	children: ComponentChildren,
+	backgroundColor?: TailwindBackgroundColor,
+	textColor?: TailwindTextColor,
 }
 
-const Button: FunctionComponent<Props> = ({id, children, ...buttonProps}: Props) => (
+const Button: FunctionComponent<Props> = ({
+	id,
+	children,
+	backgroundColor,
+	textColor,
+	...buttonProps
+}: Props) => (
 	<button
 		id={id}
-		className="py-2 px-4 bg-primary text-white font-semibold rounded-lg shadow-md hover:bg-primary/80 focus:outline-none focus:ring-2 focus:ring-primary/25 focus:ring-opacity-75"
+		className={`py-2 px-4 ${TailwindBackgroundColors[backgroundColor]} ${TailwindTextColors[textColor]} font-semibold rounded-lg shadow-md hover:bg-primary/80 focus:outline-none focus:ring-2 focus:ring-primary/25 focus:ring-opacity-75`}
 		{...buttonProps}
 	>
 		{children}
--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,6 +1,6 @@
 import Button from '../../components/Button'
 import ProgressCircle from '../../components/ProgressCircle';
-import { useState, useContext } from 'react';
+import { useContext } from 'react';
 import { TrackerContext } from '../../context/trackerContext';
 
 
@@ -23,8 +23,11 @@ export default function Home() {
 				max={100}
 				used={usedKcal}
 			/>
-			<Button id="increase" onClick={() => {setUsedKcal(usedKcal + 1)}}>
+			<Button id="increase" onClick={() => {setUsedKcal(usedKcal + 1)}} textColor='amber-400' backgroundColor='emerald-700'>
 				Increase Value
+			</Button>
+			<Button id="decrease" onClick={() => {setUsedKcal(usedKcal - 1)}} textColor='amber-400' backgroundColor='emerald-700'>
+				Decrease Value
 			</Button>
 		</main>
 	);
